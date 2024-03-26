dd48dd4dcc9b7c38945c4b6ee2b3956f002dba25
m.nosov <m.nosov@fun-box.ru>
2024-03-26 15:48:33 +0300

    Migration project to TypeScript

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-const { forgetKnownHosts } = require('.');
-const { version } = require('./package.json');
+import { forgetKnownHosts } from '.';
+import { version } from '../package.json';
 
 if (process.argv[2] === 'forget-known-hosts') {
   forgetKnownHosts();
