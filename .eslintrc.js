67a1bf74bfec8ea1835c9fcceff5c73490b746fe
m.nosov <m.nosov@fun-box.ru>
2024-03-26 15:49:20 +0300

    Update .eslint.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,25 @@
 module.exports = {
-  extends: '@funboxteam',
+  parser: '@typescript-eslint/parser',
+  extends: [
+    '@funboxteam/eslint-config',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+  ],
+  plugins: [
+    '@typescript-eslint',
+    'import',
+  ],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        project: 'tsconfig.json',
+      },
+    },
+  },
+  rules: {
+    'import/extensions': 0,
+  },
 };
